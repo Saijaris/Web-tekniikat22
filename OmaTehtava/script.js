4a871609c97f6a3d11790109ffe2b0bd447f7561
saijaris <saija.ristolainen@gmail.com>
2022-05-24 15:07:02 +0300

    events and layout manipulation added

--- OmaTehtava/script.js
+++ OmaTehtava/script.js
@@ -8,3 +8,22 @@ fetch('https://dog.ceo/api/breeds/image/random')
     '<img src="' + data.message + '"/>';
     }
 );
+
+let h2 = document.querySelector("h2");
+let teksti = document.querySelector("p");
+let body = document.querySelector("body");
+
+body.classList.add("kirkas");
+teksti.style.fontSize= "20px";
+
+h2.addEventListener("click", huomioVari);
+h2.addEventListener("mouseenter", vaihdaTausta);
+h2.addEventListener("mouseleave", vaihdaTausta);
+
+function huomioVari() {
+    h2.classList.toggle("huomio")
+}
+
+function vaihdaTausta() {
+    h2.classList.toggle("vihrea");
+}
